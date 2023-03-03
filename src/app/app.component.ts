d9c16419196f05b649e0ceaebd9b8544aa9a5717
feliciazzfx <125969563+feliciazzfx@users.noreply.github.com>
2023-03-03 15:30:31 +0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'd-anchor-basic',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class BasicComponent {
+}
